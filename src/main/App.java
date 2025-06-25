72fa8f1899f4a2ff75dd0a5b0a8ad9958587d94f
SebastianLopez <2006sacura@gmail.com>
2025-06-25 22:10:34 +0000

    leccion

--- src/main/App.java
+++ src/main/App.java
@@ -1,14 +1,23 @@
 package main;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import utils.StudentValidator;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // NO MODIFICAR ESTE CÓDIGO
-        var estudiante = StudentValidator.validarDatos();
-        System.out.println("👤 Estudiante: " + estudiante.nombre());
-        System.out.println("✉️  Correo: " + estudiante.correo());
-        // NO MODIFICAR HASTA AQUÍfdsafads
+
+        LogicaClasificacion lg = new LogicaClasificacion();
+
+        Queue<String> cola = new LinkedList<>();
+        cola.add("Ana");
+        cola.add("Luis");
+        cola.add("Pedro");
+        System.out.print(cola);
+        System.out.print(lg.invertirColaNombres(cola));
+        
+        
 
     }
 }
--- src/main/LogicaClasificacion.java
+++ src/main/LogicaClasificacion.java
@@ -15,6 +15,19 @@ public class LogicaClasificacion {
      * @return nueva cola con los nombres en orden invertido
      */
     public Queue<String> invertirColaNombres(Queue<String> cola) {
+        Stack<String> arr = new Stack<>();
+        for ( String c : cola ) {
+            while (!arr.isEmpty()) arr.push(c); {
+                String s = "";
+                while (!cola.isEmpty()) s += arr.pop(); {
+                    
+                }
+                
+                
+            }
+
+        }
+        
 
         return new LinkedList<>(Arrays.asList()); // Simulación de resultado
     }
@@ -32,8 +45,14 @@ public class LogicaClasificacion {
      * @return true si forman un palíndromo, false si no
      */
     public boolean verificarPalindromoCola(Queue<String> cola) {
+        Stack<Character> arr = new Stack<>();
+        for ( String c : cola){
+            if (c == "r" || c == "a" || c == "d" || c == "a" || c == "r") arr.push(c);
+            if ( c == "a" && arr.pop() != "r" || c == "d" && arr.pop() != " a" || c == "a" && arr.pop() != " r" ) return false;
+        }
+        
 
-        return false;
+        return arr.isEmpty();
     }
 
 }
